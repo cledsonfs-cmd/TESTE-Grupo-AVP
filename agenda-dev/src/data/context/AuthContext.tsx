a08fc2dcf51d20294ec1463b8809abd04c10a9f8
cledsonfs-cmd <cledsonfs@gmail.com>
2023-11-19 11:05:06 -0300

    19-11-23-2

--- agenda-dev/src/data/context/AuthContext.tsx
+++ agenda-dev/src/data/context/AuthContext.tsx
@@ -61,9 +61,9 @@ export function AuthProvider(props) {
         try {
             setCarregando(true)
             const resp = await firebase.auth().signInWithEmailAndPassword(email, senha)
-    
+
             await configurarSessao(resp.user)
-            router.push('/')            
+            router.push('/')
         } finally {
             setCarregando(false)
         }
@@ -72,14 +72,49 @@ export function AuthProvider(props) {
     async function cadastrar(email, senha) {
         try {
             setCarregando(true)
-            const resp = await firebase.auth().createUserWithEmailAndPassword(email, senha)
-    
-            await configurarSessao(resp.user)
-            router.push('/')            
+            const userCadastrar = {
+                email,
+                password: senha
+            }
+            const res = await fetch('http://localhost:3000/user', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userCadastrar),
+            }).then((res) => res.json())
+            .catch((err) => err);
+
+            //const resp = await firebase.auth().createUserWithEmailAndPassword(email, senha)
+            
+            //if (res.ok) {
+                console.log("-->"+res.json)
+            //  }
+            //await configurarSessao(resp.user)
+            //router.push('/')
         } finally {
             setCarregando(false)
         }
     }
+
+    // const register = async (data) => {
+    //     const config = requestConfig("POST", data);
+      
+    //     try {
+    //       const res = await fetch(api + "/usuarios", config)
+    //         .then((res) => res.json())
+    //         .catch((err) => err);
+      
+    //       if (res.ok) {
+    //         localStorage.setItem("user", JSON.stringify(res));
+    //         localStorage.setItem("token", JSON.stringify(res.token));
+    //       }
+              
+    //       return res;
+    //     } catch (error) {
+    //       console.log(error);
+    //     }
+    //   };
 
     async function loginGoogle() {
         try {
@@ -87,32 +122,32 @@ export function AuthProvider(props) {
             const resp = await firebase.auth().signInWithPopup(
                 new firebase.auth.GoogleAuthProvider()
             )
-    
+
             await configurarSessao(resp.user)
-            router.push('/')            
+            router.push('/')
         } finally {
             setCarregando(false)
         }
     }
 
-    async function logout(){
+    async function logout() {
         try {
             setCarregando(true)
             await firebase.auth().signOut()
-            await configurarSessao(null)            
+            await configurarSessao(null)
         } finally {
             setCarregando(false)
         }
     }
 
-    useEffect(()=>{
-        if(Cookies.get('admin-template-auth')){
-            const cancelar = firebase.auth().onIdTokenChanged(configurarSessao)        
+    useEffect(() => {
+        if (Cookies.get('admin-template-auth')) {
+            const cancelar = firebase.auth().onIdTokenChanged(configurarSessao)
             return () => cancelar()
         } else {
             setCarregando(false)
         }
-    },[])
+    }, [])
 
     return (
         <AuthContext.Provider value={{
@@ -120,7 +155,7 @@ export function AuthProvider(props) {
             carregando,
             cadastrar,
             login,
-            loginGoogle, 
+            loginGoogle,
             logout
         }}>
             {props.children}
